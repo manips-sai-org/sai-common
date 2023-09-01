33bf3a482c9591c1a413c7132b348d2826356741
Mikael <mjorda@stanford.edu>
2023-09-01 14:26:26 +0200

    removed all dependencies to other sai2 modules

--- examples/01-timer/main.cpp
+++ examples/01-timer/main.cpp
@@ -2,74 +2,58 @@
 #include <string>
 #include <thread>
 
-#include "Sai2Graphics.h"
-#include "Sai2Simulation.h"
 #include "timer/LoopTimer.h"
 
 using namespace std;
 
-const string world_fname = "resources/world.urdf";
-const string robot_name = "RRBot";
-
-bool fSimulationRunning = false;
-
-// sim
-void simulation(std::shared_ptr<Sai2Simulation::Sai2Simulation> sim);
+// second thread function
+void run2();
 
 int main(int argc, char** argv) {
-	cout << "Loading URDF world model file: " << world_fname << endl;
-
-	// load simulation world
-	auto sim = make_shared<Sai2Simulation::Sai2Simulation>(world_fname);
-
-	// load graphics scene
-	auto graphics = make_shared<Sai2Graphics::Sai2Graphics>(world_fname);
-	graphics->setBackgroundColor(0.2, 0.2, 0.2);
-
 	cout << endl
-		 << "This example simulates a double pendulum, using a timer to set "
-			"the simulation to real time after a 0.5 sec initial wait."
+		 << "This example runs 2 threads with one timer each at different frequencies."
 		 << endl
 		 << endl;
 
-	// start the simulation
-	thread sim_thread(simulation, sim);
+	// start the second thread
+	thread thread2(run2);
 
-	// while window is open:
-	while (graphics->isWindowOpen()) {
-		// update graphics.
-		graphics->updateRobotGraphics(robot_name, sim->getJointPositions(robot_name));
-		graphics->renderGraphicsWorld();
+	// create a loop timer
+	Sai2Common::LoopTimer timer(50.0); // 50Hz timer
+	timer.initializeTimer(1e6); // 1ms pause before starting loop
+
+	// run for 1.5 seconds
+	while (timer.elapsedTime() < 1.5) {
+		// wait the correct amount of time
+		timer.waitForNextLoop();
+
+		std::cout << "Main thread at " << timer.elapsedTime() << " seconds." << std::endl;
 	}
+	timer.stop();
 
-	// stop simulation
-	fSimulationRunning = false;
-	sim_thread.join();
+	// wait for second thread to finish
+	thread2.join();
+
+	// print main timer info
+	std::cout << "main thread timer info:" << std::endl;
+	timer.printInfoPostRun();
 
 	return 0;
 }
 
 //------------------------------------------------------------------------------
-void simulation(std::shared_ptr<Sai2Simulation::Sai2Simulation> sim) {
+void run2() {
 	// create a loop timer
-	Sai2Common::LoopTimer timer(1.0/sim->timestep());
+	Sai2Common::LoopTimer timer(5.0);
 	timer.initializeTimer(0.5*1e9); // 0.5 s pause before starting loop
 
-	fSimulationRunning = true;
-	while (fSimulationRunning) {
+	while (timer.elapsedTime() < 3.0) {
 		// wait the correct amount of time
 		timer.waitForNextLoop();
 
-		// integrate forward
-		sim->integrate();
+		std::cout << "Second thread at " << timer.elapsedTime() << " seconds." << std::endl;
 	}
 
-	// display controller run frequency at the end
-	double end_time = timer.elapsedTime();
-    std::cout << "\n";
-    std::cout << "Simulated time            : " << sim->time() << " seconds\n";
-    std::cout << "Simulation Loop run time  : " << end_time << " seconds\n";
-    std::cout << "Simulation Loop updates   : " << timer.elapsedCycles() << "\n";
-    std::cout << "Simulation Loop frequency : " << timer.elapsedCycles()/end_time << "Hz\n";
-
+	timer.printInfoPostRun();
+	timer.stop();
 }
