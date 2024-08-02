18313d125e1d3c719a1fb59f2764e9801d3294b9
Mikael Jorda <mikael.jorda@gmail.com>
2024-08-02 15:14:02 +0200

    updated comments on LoopTimer

--- src/timer/LoopTimer.h
+++ src/timer/LoopTimer.h
@@ -1,5 +1,3 @@
-// LoopTimer.h
-
 #ifndef SAI_LOOPTIMER_H_
 #define SAI_LOOPTIMER_H_
 
@@ -12,11 +10,27 @@
 
 namespace Sai2Common {
 
-/** \brief Accurately time a loop to set frequency.
+/**
+ * @brief This class implements a precise timer to run a loop at a specified
+ * frequency, and provide monitoring options for the loop runtimes.
+ * There are 2 ways of using it:
+ * 1 - Explicitly implement a while loop and call waitForNextLoop() in it
+ * (ideally at the start of the loop) that will wait for the correct amount of
+ * time to reach the desired frequency 2 - Use the run() function, which will
+ * run a loop calling a user defined callback function at the specified
+ * frequency
  *
  */
 class LoopTimer {
 public:
+	/**
+	 * @brief Construct a new Loop Timer with the specified frequency and
+	 * initial wait time before starting the first loop
+	 *
+	 * @param frequency The frequency in Hz
+	 * @param initial_wait_nanoseconds The initial wait time in nanoseconds
+	 * before starting the first loop
+	 */
 	LoopTimer(double frequency, unsigned int initial_wait_nanoseconds = 0);
 
 	~LoopTimer() = default;
@@ -28,15 +42,18 @@ public:
 
 	void setTimerName(const std::string& name) { timer_name_ = name; }
 
-	/** \brief Set the loop frequency
-	 * \param frequency The loop frequency that will be used for
-	 * LoopTimer::run()
+	/**
+	 * @brief Set the loop frequency
+	 *
+	 * @param frequency The frequency in Hz
 	 */
 	void resetLoopFrequency(double frequency);
 
-	/** \brief Initialize the timing loop, if using your own while loop. call
-	 * before waitForLoop. \param initial_wait_nanoseconds The delay before
-	 * waitForNextLoop will return the first time
+	/**
+	 * @brief Reinitialize the timer.
+	 *
+	 * @param initial_wait_nanoseconds The wait time in nanoseconds before the
+	 * timer starts again
 	 */
 	void reinitializeTimer(unsigned int initial_wait_nanoseconds = 0);
 
@@ -46,17 +63,44 @@ public:
 	 * and true otherwise. If no overtime monitoring is enabled, this will
 	 * return true if the timer waited and false otherwise.
 	 */
+
+	/**
+	 * @brief Wait for next loop. Use if a while loop is explicitely implemented
+	 * in order to wait the correct amount of time. Not needed if using
+	 * LoopTimer::run(). If overtine monitoring is enabled, this function will
+	 * return false in case the overtime monitoring conditions are not satisfied
+	 * and true otherwise. If no overtime monitoring is enabled, this will
+	 * return true if the timer waited and false otherwise.
+	 *
+	 * @return true if the timer waited, or if overtime monitoring is enabled
+	 * and the overtime conditions are satisfied
+	 * @return false if the timer did not wait, or if overtime monitoring is
+	 * enabled and the overtime conditions are not satisfied
+	 */
 	bool waitForNextLoop();
 
-	/** \brief Number of full loops completed since calling run. */
+	/**
+	 * @brief Number of full loops completed since calling run, or number of
+	 * times the waitForNextLoop function was called
+	 */
 	unsigned long long elapsedCycles();
 
-	/** \brief Elapsed computer time since calling initializeTimer() or run() in
-	 * seconds. */
+	/**
+	 * @brief Elapsed real time (from computer cpu clock) since the timer
+	 * started
+	 *
+	 * @return time in seconds
+	 */
 	double elapsedTime();
 
-	/** \brief Elapsed simulation time since calling initializeTimer() or run()
-	 * in seconds. */
+	/**
+	 * @brief Elapsed "simulation" time since the timer started. This
+	 * corresponds to the theoretical time that should have been elapsed if the
+	 * loop was running at the desired frequency, and is computed from the
+	 * number of epapsed cycles and the frequency of the loop
+	 *
+	 * @return time in seconds
+	 */
 	double elapsedSimTime();
 
 	/**
@@ -82,20 +126,46 @@ public:
 		const double percentage_overtime_loops_allowed,
 		const bool print_warning = false);
 
-	/** \brief Print the loop frequency and the average loop time. */
+	/**
+	 * @brief Print a summary of the loop timer at the current time, or until
+	 * the stip function was called. It will print info such as the desired
+	 * running frequency, the actual one, the elapsed time and elapsed sim time,
+	 * and some overtime monitoring info.
+	 *
+	 */
 	void printInfoPostRun();
 
 	/** \brief Run a loop that calls the user_callback(). Blocking function.
 	 * \param userCallback A function to call every loop.
 	 */
+
+	/**
+	 * @brief Starts a loop that calls the user_callback() function at the
+	 * specified frequency. This is less tested than the other method of
+	 * implementing explicitely the while loop and using the waitForNextLoop()
+	 * function. This function is blocking and will only return when the stop()
+	 * function is called.
+	 *
+	 * @param userCallback the callback function to run at the specified
+	 * frequency
+	 */
 	void run(void (*userCallback)(void));
 
 	/** \brief Stop the loop, started by run(). Use within callback, or from a
 	 * seperate thread. */
-	void stop();
 
-	/** \brief Add a ctr-c exit callback.
-	 * \param userCallback A function to call when the user presses ctrl-c.
+	/**
+	 * @brief Stop the loop started by the run() function. This function needs
+	 * to be called from within the callback function, or from a separate thread
+	 * if using the run() function.
+	 *
+	 */
+	void stop() { running_ = false; }
+
+	/**
+	 * @brief Set the Ctrl C Handler object (untested)
+	 * 
+	 * @param userCallback A function to call when the user presses ctrl-c.
 	 */
 	static void setCtrlCHandler(void (*userCallback)(int)) {
 		struct sigaction sigIntHandler;
@@ -105,8 +175,11 @@ public:
 		sigaction(SIGINT, &sigIntHandler, NULL);
 	}
 
-	/** \brief Set the thread to a priority of -19. Priority range is -20
-	 * (highest) to 19 (lowest) */
+	/**
+	 * @brief Set the thread to a priority of -19. Priority range is -20
+	 * (highest) to 19 (lowest). Untested.
+	 * 
+	 */
 	static void setThreadHighPriority();
 
 	/** \brief Set the thread to real time (FIFO). Thread cannot be preempted.
